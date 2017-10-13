1d186f9ed19dc3c5f9e33e46d1eaf5aa697f1070
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-10-13 18:29:16 -0500

    created file structure

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+// requiring controller
+const petController = require('./controllers/petApp')
+
+
+// setting view engine
+app.set('view engine', 'ejs');
+app.set('views' __dirname + '/views');
+
+// allow parsing of req.body
+app.use(bodyParser.urlencoded({extended: false}));
+
+//pointing to public asset folder
+app.use(express.static('public'));
+
+app.use(methodOverride('_method'));
+
+app.use('/pet', petControlller);
+
+//setting port to listen on
+app.listen(3000, () => {
+	console.log('Listening on port 3000')
+});
---
+++ models/petApp.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const PetSchema = new mongoose.Schema({
+	
+})
